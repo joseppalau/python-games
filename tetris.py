ec206e82c52ca6a7c7247b75d56dbed20a0c236d
administrador <jeppalau83@gmail.com>
2021-02-04 09:23:04 +0100

    shapes and colores set

--- tetris.py
+++ tetris.py
@@ -22,7 +22,48 @@ BLUE = (0,102,204)
 pygame.draw.rect(screen, BLUE, (top_lef_x,top_left_y,play_width,play_height))
 pygame.display.update()
 
-shape_colors = {}
+
+S = [[],[]]
+Z = [[],[]]
+I = [[],[]]
+O = [[]]
+J = [[],[],[],[]]
+L = [[],[],[],[]]
+T = [[],[],[],[]] 
+
+shapes = [S,Z,I,O,J,L,T]
+shape_colors = [(0,255,0),(255,0,0),(0,255,255),(255,255,0),(255,165,0),(0,0,255),(128,0,128)]
+
+class Piece(object):
+	def __init__(self, x, y, shape):
+		self.x = x
+		self.y = y
+		self.shape = shape
+		self.color = shape_colors[shapes.index(shape)]
+		self.rotation = 0
+
+def create_grid(locked_pos = {}):
+	grid = [ (0,0,0) for _ in range(10) for _ in range(20)]
+
+	for i in range(len(grid)):
+		for j in range(len(grid[i])):
+			if (j,i) in locked_pos:
+				c = locked_pos[(j,i)]
+				grid[i][j] = c
+	return grid			
+
+def draw_grid(surface):
+	surface.fill((0,0,0))
+	
+	pygame.font.init()
+	font = pygame.font.SysFont('comicsans', 69)
+	label = font.render('Tetris', 1, (255,255,255))
+
+	surface.blit(lable, (top_lef_x + play_width/2 - label.get_with()//2, 30)
+
+def get_shape():
+	return random.choice(shapes)
+
 
 while True:
 
