ff43d85aebb418cadb41763312a8cdfa5331c0ed
administrador <jeppalau83@gmail.com>
2021-02-25 18:35:10 +0100

    move function added to player class

--- online_chess/client.py
+++ online_chess/client.py
@@ -4,34 +4,62 @@ WITH = 500
 HEIGHT = 500
 
 screen = pygame.display.set_mode(size=(WITH, HEIGHT))
-pygame.display.set_caption(title='CLIENT')
+pygame.display.set_caption('CLIENT')
 
 clientNumber = 0
 
 class Player():
 	def __init__(self, x, y, width, height, color):
 		self.x = x
-		self.y = yself.width = width
+		self.y = y
+		self.width = width
 		self.height = height
 		self.color = color
 		self.rect =(x,y,width,height) 
+		self.speed = 3
 
 	def draw(self, screen):
 		pygame.draw.rect(screen, self.color, self.rect)	
 
+	def move(self, dx, dy):
+		self.x += dx * self.speed
+		self.y += dy * self.speed
+		self.rect=(self.x, self.y, self.width, self.height)		
 
-def redraw_screen():
+
+def redraw_screen(screen, player):
 	screen.fill(color=(255,255,255))
+	player.draw(screen)
 	pygame.display.update()
 
-def main():
+def main(screen):
 	run = True
+	p = Player(50,50,100,100,(0,255,0))
+	clock = pygame.time.Clock()
 
 	while run:
+		clock.tick(60)
+		dx = 0
+		dy = 0
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				run = False
 				pygame.quit()
+
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_LEFT:
+					dx = -1
+				if event.key == pygame.K_RIGHT:
+					dx = 1
+				if event.key == pygame.K_UP:
+					dy = -1
+				if event.key == pygame.K_DOWN:	
+					dy = 1
+
+		p.move(dx, dy)
+		redraw_screen(screen, p)	
+
+main(screen)			
 
 
 
