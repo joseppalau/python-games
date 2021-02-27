f0d4ff50a6a074e4d6e2d4ea971a5f4160975122
administrador <jeppalau83@gmail.com>
2021-02-27 08:28:40 +0100

    server and network files added

---
+++ online_chess/server.py
@@ -0,0 +1,47 @@
+import socket
+import _thread as th
+import sys
+
+server = '192.168.1.1'
+port = 5555
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+	s.bind((server, port))
+
+except socket.error as e:
+		print('some error')
+		str(e)
+
+else:
+	s.listen(2)
+	print('Wating for a connection, Server Started')
+
+def threaded_client(conn):
+	conn.send(str.encode('Connected'))
+	reply = ''
+	while True:
+		try:
+			data = conn.recv(2048)
+			reply = data.decode('utf-8')
+		
+		except:
+			print('Non data. Disconnected')
+			break
+
+		else:
+			print('Recieved: ', reply)	
+			print('Sending: ', reply)
+			conn.sendall(str.encode(reply))	
+
+	print('Lost connection')	
+	conn.close()	
+
+
+while True:
+	conn, addr = s.accept()
+	print('Connected to:', addr)
+
+	th.start_new_thread(threaded_client(conn), (conn,))
+
